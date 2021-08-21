9073a62f90e6d25c4d4e214dc5f1f9006630642d
CyberBreach <shrivastavapiyush781@gmail.com>
2021-08-21 16:17:40 +0530

    Nav Bar
    Changed Navbar made it responsive

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//DARK/LIGHt MODE
+function changeMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+// HAMBURGER ANIMATION
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+    });
+}
+navSlide();
